ae041ef2eb90c48c0dc7310333ad903b16f7789a
pthienquan1 <quan01011997123@gmail.com>
2022-07-02 15:45:28 +0700

    Add folder src and add folder public in src

---
+++ src/public/js/checkout.js
@@ -0,0 +1,64 @@
+var stripe = Stripe('pk_test_51KsIjtFdigjD3itZzOTRrMGOJ4xAC1p8JuCt5cFHzfU0Q5n1gYXPzBy0Os9riOn7W4KUmV7GIcPYqkVINt0CDKG400jUNclm4D');
+
+var $form = $('#checkout-form');
+
+var elements = stripe.elements();
+
+var cardElement = elements.create('card', {
+    hidePostalCode: true,
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        lineHeight: '40px',
+        fontWeight: 300,
+        fontFamily: 'Helvetica Neue',
+        fontSize: '15px',
+  
+        '::placeholder': {
+          color: '#CFD7E0',
+        },
+      },
+    }
+});
+
+cardElement.mount('#card-element');
+
+$form.submit(function(event) {
+    $('#charge-error').addClass('d-none');
+    $form.find('button').prop('disabled', true);
+
+    var options = {
+        name: $('#fullName').val(),
+        address_line1: $('#address').val(),
+        address_city: $('#city').val(),
+        address_state: $('#state').val(),
+        address_zip: $('#zipCode').val(),
+        address_country: $('#country').val(),
+    };
+
+    stripe.createToken(cardElement, options).then(stripeResponseHandler);
+    return false;
+});
+
+function stripeResponseHandler(response) {
+    if(response.token) { // Token was created!
+
+        // Get the token ID:
+        var token = response.token.id;
+        
+        // Insert token into form so it gets submitted to the server:
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+        // Submit the form:
+        $form.get(0).submit();
+
+    } else if(response.error) { // PROBLEM
+
+        //Show error msg on form
+        $('#charge-error').text(response.error.message);
+        $('#charge-error').removeClass('d-none');
+        $form.find('button').prop('disabled', false);   // Re-enable submission
+
+    }    
+};
